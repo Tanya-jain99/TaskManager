fc850a38ed2c727240e00e40d80a1474dee8bf62
Tanya Jain <jaintanya999@gmail.com>
2026-04-14 15:10:44 +0530

    chore: compose,hilt,ksp config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
+    alias(libs.plugins.hotswan.compiler)
 }
 
 android {
-    namespace = "com.example.planner"
+    namespace = "com.tanya.planner"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.planner"
-        minSdk = 24
+        applicationId = "com.tanya.planner"
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -40,6 +42,10 @@ android {
 }
 
 dependencies {
+    val hilt_version = "2.59.1"
+    val room_version = "2.8.4"
+    val nav_version = "2.9.7"
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,10 +55,22 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Room dependencies
+    implementation("androidx.room:room-runtime:${room_version}")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:${room_version}")
+
+    // Hilt dependencies
+    implementation("com.google.dagger:hilt-android:2.59.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.59.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+
+    // Database module
+    implementation(project(":database"))
 }
